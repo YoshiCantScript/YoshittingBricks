0cec5d8db95bc801ee4daf3650107cf296ddb151
YoshiCantScript <149406088+YoshiCantScript@users.noreply.github.com>
2024-02-08 22:43:45 +0100

    Serach testing

--- YoshiCooking/src/main/kotlin/com/yoshi/YoshiProvider.kt
+++ YoshiCooking/src/main/kotlin/com/yoshi/YoshiProvider.kt
@@ -24,4 +24,15 @@ open class StreamingcommunityProvider : MainAPI() {
     private val userAgent =
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 }
-   
+   override susped search(query: string): List<SearchResponse> {
+    val url = "$mainUrl/search"
+    val soup = app.get(
+        ulr, params = mapOf("q" to query), headers = mapOf(
+             "X-Inertia" to "true", "X-Inertia-Version" to "16d7fef7dd27890ede802c00747e79cb"
+            )
+        ).text
+        val responseJson = parseJson<SearchResponseJson>(soup)
+        return responseJson.props.titles.map { it.toSearchResult() }
+    }
+
+        
