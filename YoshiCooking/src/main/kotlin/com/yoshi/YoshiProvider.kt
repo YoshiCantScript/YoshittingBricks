f211fc08c9ad2d45d935f4f5167c564064d63406
YoshiCantScript <149406088+YoshiCantScript@users.noreply.github.com>
2024-02-10 23:49:44 +0100

    Update YoshiProvider.kt

--- YoshiCooking/src/main/kotlin/com/yoshi/YoshiProvider.kt
+++ YoshiCooking/src/main/kotlin/com/yoshi/YoshiProvider.kt
@@ -33,7 +33,7 @@ class StreamingcommunityProvider : MainAPI() {
       "$mainUrl/browse/trending"
    ),   
     
-      
+   ) 
     
     private val userAgent =
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
